7059c14a095ed6f00bd49a67e0b2b2a9063d8722
Henrique Medeiros <henriquemed65@gmail.com>
2023-10-23 15:43:29 -0300

    Co-authored-by: Júlia Vasconcelos

---
+++ src/components/Display.jsx
@@ -0,0 +1,27 @@
+const Display = () => {
+    return (
+        <div>
+            <div className='display-row'>
+                <div>
+                    <div className='display-label'>
+                        <p className= "header">Tip Amount</p>
+                        <p className= "unit">/ person</p>
+                    </div>
+                    <div className="display-amt">
+                        <p className="value">$0.00</p>
+                    </div>
+                    <div className='display-label'>
+                        <p className= "header">Total</p>
+                        <p className= "unit">/ person</p>
+                    </div>
+                    <div className="display-amt">
+                        <p className="value">$0.00</p>
+                    </div>
+                </div>
+                <button className="btn">Reset</button>
+            </div>
+        </div>
+    )
+}
+
+export default Display
